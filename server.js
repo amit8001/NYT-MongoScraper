1b482f61348f791e875dcaa9922f6846cb356d86
Amitav Panda <36935650+amit8001@users.noreply.github.com>
2018-09-18 21:17:35 -0400

    changed mongo conn for heroku deployment

--- server.js
+++ server.js
@@ -29,12 +29,14 @@ partialsDir: __dirname+ "/views/layouts/partials"
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoNewsscraper";
+var MONGODB_URI = process.env.MONGODB_URI;
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI || "mongodb://localhost/mongoNewsscraper", {
+  useMongoClient: true
+});
 
 
 // Start the server
